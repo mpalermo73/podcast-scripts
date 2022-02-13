4c3b5dc8a0263cae7e6dad7e8f0bcf980b320202
Matt <matt.palermo@gmail.com>
2022-02-13 14:56:35 -0500

    not functional

---
+++ update-me-my-demon-and-i.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+URL_RSS="https://anchor.fm/s/e09c708/podcast/rss"
+PRETTY_NAME="Me, My Demon, and I"
+GOOD_REGEX="Episode [0-9]"
+
+
+
+DEBUG=TRUE
+JUST_TEST=TRUE
+NO_SLACK=TRUE
+# UPDATE_SYNCTHING=TRUE
+
+
+source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
+
+CurlFeed
+
+for LINE in ${EPISODES} ; do
+
+  LINE=$(echo "${LINE}" | sed 's/<\!\[CDATA\[//g;s/\]\]>//g')
+  # echo "LINE: ${LINE}"
+
+  eval "${LINE}"
+
+  if [ "${PUBDATE}" -a "${EPURL}" -a "${TITLE}" -a "${IMAGE}" -a "${SEASON}" -a "${EPISODE}" ] ; then
+
+    if [[ "${TITLE}" =~ ${GOOD_REGEX} ]] && [[ "${TYPE}" =~ "full" ]] ; then
+
+      [ ${DEBUG} ] && echo "PASS regex: \"${TITLE}\""
+
+      [ ${#EPISODE} -eq 1 ] && EPISODE="0${EPISODE}"
+      [ ${#SEASON} -eq 1 ] && SEASON="0${SEASON}"
+
+      TITLE="${SEASON}${EPISODE} - ${TITLE}"
+
+      DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}"
+
+    else
+      echo "---------"
+      [ ${DEBUG} ] && echo "FAIL regex: \"${TITLE}\""
+      # DumpFound
+      echo "---------"
+    fi
+    UnsetThese
+  fi
+
+done
