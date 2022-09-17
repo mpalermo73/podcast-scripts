eb60f4a00db2b9f3a2dcbc2d232fea2d4f6b5c18
Matt <matt.palermo@gmail.com>
2022-09-17 10:36:50 -0400

    the orbiting human circus

---
+++ update-the-orbiting-human-curcus.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+URL_RSS="http://feeds.wnyc.org/orbitinghumancircus"
+PRETTY_NAME="The Orbiting Human Circus"
+GOOD_REGEX="^[0-9]+"
+
+
+# DEBUG=TRUE
+# JUST_TEST=TRUE
+# NO_SLACK=TRUE
+# UPDATE_SYNCTHING=TRUE
+
+
+source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
+
+CurlFeed
+
+for LINE in ${EPISODES} ; do
+
+  eval "${LINE}"
+
+  if [ "${PUBDATE}" -a "${EPURL}" -a "${TITLE}" -a "${TYPE}" -a "${SEASON}" -a "${EPISODE}" ] ; then
+    if [ "${TYPE}" == "full" ] ; then
+
+      [ ${DEBUG} ] && echo "PASS regex: \"${TITLE}\""
+
+      [ ${#SEASON} -eq 1 ] && SEASON="0${SEASON}"
+      [ ${#EPISODE} -eq 1 ] && EPISODE="0${EPISODE}"
+
+      TITLE="${SEASON}${EPISODE} - $(echo "${TITLE}" | sed 's/.*— \(.*\)/\1/')"
+
+      DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}"
+    fi
+
+    UnsetThese
+  fi
+
+done
