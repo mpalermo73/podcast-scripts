f512b8f124f943ba1de704fc8272cc4565ebf9a9
Matt <matt.palermo@gmail.com>
2023-12-29 15:25:15 -0500

    this is broke but whatever

--- update-doctor-who.sh
+++ update-doctor-who.sh
@@ -6,8 +6,8 @@ PRETTY_NAME="Doctor Who"
 
 
 
-# DEBUG=TRUE
-# JUST_TEST=TRUE
+DEBUG=TRUE
+JUST_TEST=TRUE
 # NO_SLACK=TRUE
 # UPDATE_SYNCTHING=TRUE
 
@@ -21,9 +21,25 @@ for LINE in ${EPISODES} ; do
 
   if [ "${PUBDATE}" -a "${EPURL}" -a "${TITLE}" -a "${IMAGE}" -a "${EPISODE}" ] ; then
 
-    if [[ "${TYPE}" =~ "full" ]] ; then
+    [ ${DEBUG} ] && echo "---------"
+
+    if [[ ! "${TITLE}" =~ Trailer ]] ; then
+
 
       [ ${DEBUG} ] && echo "PASS regex: \"${TITLE}\""
+
+
+      PART=$(echo "${TITLE}" | sed 's/.*[pP][aA][rR][tT] \([a-zA-Z]\+\).*/\1/ ; s/^[0-9]\+ // ; s/[oO][nN][eE]/1/ ; s/[tT][wW][oO]/2/ ; s/[tT][hH][rR][eE][eE]/3/ ; s/[fF][oO][uU][rR]/4/ ; s/[fF][iI][vV][eE]/5/ ; s/[sS][iI][xX]/6/ ; s/[sS][eE][vV][eE][nN]/7/ ;  ; s/[eE][iI][gG][hH][tT]/8/ ; s/[nN][iI][nN][eE]/9/')
+      # EPISODE=$(echo "${TITLE}" | sed 's/^\(0-9]\+\)/\1/')
+      TITLE=$(echo ${TITLE} | sed 's/^[0-9]\+ \(.*\) part.*/\1/')
+      
+      if [ "${PART}" ] ; then
+        EPISODE="$(echo "${TITLE}" | sed 's/^\(0-9]\+\)/\1/').${PART}"
+      else
+        EPISODE="$(echo "${TITLE}" | sed 's/^\(0-9]\+\)/\1/')"
+      fi
+
+      [ ${#PART} -eq 1 ] && PART="0${PART}"
 
       [ ${#EPISODE} -eq 1 ] && EPISODE="00${EPISODE}"
       [ ${#EPISODE} -eq 2 ] && EPISODE="0${EPISODE}"
@@ -34,12 +50,15 @@ for LINE in ${EPISODES} ; do
       DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}"
 
     else
-      echo "---------"
-      [ ${DEBUG} ] && echo "FAIL regex: \"${TITLE}\" ${TYPE}"
-      # DumpFound
-      echo "---------"
+      if [ ${DEBUG} ] ; then
+        echo "FAIL regex: \"${TITLE}\" ${TYPE}"
+        DumpFound
+      fi
+      UnsetThese
     fi
+
     UnsetThese
+    [ ${DEBUG} ] && echo -e "---------\n\n"
   fi
 
 done
