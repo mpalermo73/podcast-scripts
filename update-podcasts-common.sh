a07b7c4ec5657fd27cdde54b1bd46c9a7af7134c
Matt <matt.palermo@gmail.com>
2024-01-21 10:51:23 -0500

    cleaner copy to tanks

--- update-podcasts-common.sh
+++ update-podcasts-common.sh
@@ -16,8 +16,8 @@ TANK_MEDIA="${MOUNT_MEDIA}/MUSIC/Podcasts/${PRETTY_NAME}"
 MOUNT_SYNCTHING="/mnt/syncthing"
 TANK_SYNCTHING="${MOUNT_SYNCTHING}/${PRETTY_NAME}"
 
-TANK="/usr/local/Podcasts/${PRETTY_NAME}"
-PODCAST_ALBUM_ART="${TANK}/${PRETTY_NAME}.jpg"
+TANK_LOCAL="/usr/local/Podcasts/${PRETTY_NAME}"
+PODCAST_ALBUM_ART="${TANK_LOCAL}/${PRETTY_NAME}.jpg"
 QUALITY=100
 MAX_DIMENSION="1000"
 MAX_SIZE="1024"
@@ -26,7 +26,7 @@ ITEM_COUNT="${ITEM_COUNT:-0}"
 
 [ ! ${DEBUG} ] && WGET_DEBUG="--quiet"
 # [ ! "${DATE_MIN}" ] && DATE_MIN=$(date -d "- 100 year" "+%F")
-[ ! -d "${TANK}" ] && mkdir -p "${TANK}"
+[ ! -d "${TANK_LOCAL}" ] && mkdir -p "${TANK_LOCAL}"
 
 if [ $# -eq 1 ] && [[ "$1" =~ ^[rR][eE][tT][aA][gG]$ ]] ; then
   DO_RETAG=TRUE
@@ -40,35 +40,9 @@ function GetPodcastImage() {
 
     curl -sL "${IMAGE}" | convert -resize ${MAX_DIMENSION} -define jpeg:extent=${MAX_SIZE}K - "${PODCAST_ALBUM_ART}"
 
-    # TMP_ART="${PODCAST_ALBUM_ART}.tmp"
-
-    # wget ${WGET_DEBUG} "${IMAGE}" -O "${TMP_ART}"
-
-    # eval "$(/usr/bin/vendor_perl/exiftool -q -f "${TMP_ART}" \
-    #   -p 'IMAGE_WIDTH="$imageWidth"' \
-    #   -p 'IMAGE_HEIGHT="$imageHeight"')"
-
-    # if [ ${DEBUG} ] ; then
-    #   echo -e "\\tIMAGE_WIDTH: \"${IMAGE_WIDTH}\""
-    #   echo -e "\\tIMAGE_HEIGHT: \"${IMAGE_HEIGHT}\""
-    # fi
-
-    # PODCAST_ALBUM_ART="/tmp/${PRETTY_NAME}.jpg"
-
-    # convert -resize ${MAX_DIMENSION}x${MAX_DIMENSION}! -quality ${QUALITY} "${TMP_ART}" "${PODCAST_ALBUM_ART}"
-
-    # while [ $(ls -sk "${PODCAST_ALBUM_ART}" | awk '{print $1}') -gt ${MAX_SIZE} ] ; do
-    #   convert -resize ${MAX_DIMENSION}x${MAX_DIMENSION}! -quality ${QUALITY} "${TMP_ART}" "${PODCAST_ALBUM_ART}"
-    #   QUALITY=$(( ${QUALITY} - 1))
-    # done
-
-    # [ -f "${TMP_ART}" ] && rm -f "${TMP_ART}"
-
-    # unset TMP_ART
-
   elif [ ! -f "${PODCAST_ALBUM_ART}" ]; then
     echo -e "IMAGE var is not set and \"${PODCAST_ALBUM_ART}\" not found"
-    echo -e "Can't tag ${TANK}/${OUTFILE}"
+    echo -e "Can't tag ${TANK_LOCAL}/${OUTFILE}"
     exit 1
   fi
 }
@@ -79,33 +53,35 @@ function CopyEpisode() {
 
   [ ${DEBUG} ] && echo "CopyEpisode - \"$1\""
 
-  # TODO:
-  # -> % cmp -s /mnt/syncthing/Trailer\ Park\ Boys/001\ -\ Can\'t\ Get\ You\ Out\ Of\ My\ Head.mp3 001\ -\ Can\'t\ Get\ You\ Out\ Of\ My\ Head.mp3 ; echo $?
-  # 0
-
-
   for MTANK in ${TANK_MEDIA} ${TANK_SYNCTHING} ; do
 
     case ${MTANK} in
       ${TANK_SYNCTHING})
-        # if [ ${UPDATE_SYNCTHING} ] && [ ${NEW_EPISODE} ] ; then
         if [ ${UPDATE_SYNCTHING} ] ; then
-          [ ! "$(mount | grep ${MOUNT_SYNCTHING})" ] && mount ${MOUNT_SYNCTHING}
           [ ${DEBUG} ] && echo "UPDATING ${MTANK}"
-          [ ${DEBUG} ] && [ -f "${MTANK}/$(basename "$1")" ] && echo "EXISTS - ${MTANK}/$(basename "$1")"
-          [ ${DEBUG} ] && [ ! -f "${MTANK}/$(basename "$1")" ] && echo "COPYING $(basename "$1") --> ${MTANK}"
-          [ ! -d "${MTANK}" ] && mkdir -p "${MTANK}"
-          [ ! -f "${MTANK}/$(basename "$1")" ] && cp -a "$1" "${MTANK}/$(basename "$1")"
+          [ ! "$(mount | grep ${MOUNT_SYNCTHING})" ] && mount ${MOUNT_SYNCTHING}
+          
+          if [ ! -f "${MTANK}/$(basename "$1")" ] ; then
+            [ ${DEBUG} ] && echo "COPYING $(basename "$1") --> ${MTANK}"
+            [ ! -d "${MTANK}" ] && mkdir -p "${MTANK}"
+            cp -a "$1" "${MTANK}/$(basename "$1")"
+          else
+            [ ${DEBUG} ] && echo "EXISTS - ${MTANK}/$(basename "$1")"
+          fi
         fi
         ;;
       ${TANK_MEDIA})
         if [ ! ${NO_UPDATE_REMOTE} ] ; then
           [ ${DEBUG} ] && echo "UPDATING ${MTANK}"
           [ ! "$(mount | grep ${MOUNT_MEDIA})" ] && mount ${MOUNT_MEDIA}
-          [ ${DEBUG} ] && [ -f "${MTANK}/$(basename "$1")" ] && echo "EXISTS - ${MTANK}/$(basename "$1")"
-          [ ${DEBUG} ] && [ ! -f "${MTANK}/$(basename "$1")" ] && echo "COPYING $(basename "$1") --> ${MTANK}"
-          [ ! -d "${MTANK}" ] && mkdir -p "${MTANK}"
-          [ ! -f "${MTANK}/$(basename "$1")" ] && cp -a "$1" "${MTANK}/$(basename "$1")"
+
+          if [ ! -f "${MTANK}/$(basename "$1")" ] ; then
+            [ ${DEBUG} ] && echo "COPYING $(basename "$1") --> ${MTANK}"
+            [ ! -d "${MTANK}" ] && mkdir -p "${MTANK}"
+            [ ! -f "${MTANK}/$(basename "$1")" ] && cp -a "$1" "${MTANK}/$(basename "$1")"
+          else
+            [ ${DEBUG} ] && echo "EXISTS - ${MTANK}/$(basename "$1")"
+          fi
         fi
         ;;
       *)
@@ -122,13 +98,12 @@ function EpisodeTagging() {
 
   YEAR=$(date -d "$PUBDATE" +%Y)
 
-  # [ "${IMAGE}" ] && GetPodcastImage
   GetPodcastImage
 
   if [ $# -eq 1 ] && [ -f "$1" ] ; then
     TAGGING_FILE="$1"
   else
-    TAGGING_FILE="${TANK}/${OUTFILE}"
+    TAGGING_FILE="${TANK_LOCAL}/${OUTFILE}"
   fi
 
   [ ${DEBUG} ] && echo "EpisodeTagging \"$TAGGING_FILE\""
@@ -162,7 +137,7 @@ function GetEpisode() {
   PUBDATE=${4}
   FILEURL=${5}
 
-  wget ${WGET_DEBUG} "${FILEURL}" -O "${TANK}/${OUTFILE}"
+  wget ${WGET_DEBUG} "${FILEURL}" -O "${TANK_LOCAL}/${OUTFILE}"
 }
 
 
@@ -226,28 +201,28 @@ function ProcessEpisode() {
   if [ ${DO_RETAG} ] ; then
 
     for MTANK in ${TANK_MEDIA} ${TANK_SYNCTHING} ; do
-      if [ -f "${TANK}/$(basename "${OUTFILE}")" ] ; then
-        echo "RETAGGING ${TANK}/$(basename "${OUTFILE}")"
-        EpisodeTagging "${TANK}/$(basename "${OUTFILE}")"
-        [ -f "${MTANK}/$(basename "${OUTFILE}")" ] && cp -av "${TANK}/$(basename "${OUTFILE}")" "${MTANK}/$(basename "${OUTFILE}")"
+      if [ -f "${TANK_LOCAL}/$(basename "${OUTFILE}")" ] ; then
+        echo "RETAGGING ${TANK_LOCAL}/$(basename "${OUTFILE}")"
+        EpisodeTagging "${TANK_LOCAL}/$(basename "${OUTFILE}")"
+        [ -f "${MTANK}/$(basename "${OUTFILE}")" ] && cp -av "${TANK_LOCAL}/$(basename "${OUTFILE}")" "${MTANK}/$(basename "${OUTFILE}")"
       fi
     done
 
   else
 
-    if [ ! -f "${TANK}/${OUTFILE}" ] ; then
+    if [ ! -f "${TANK_LOCAL}/${OUTFILE}" ] ; then
       [ ${DEBUG} ] && echo "NEED: ${OUTFILE}"
 
       GetEpisode "${TRACK}" "${TITLE}" "${OUTFILE}" "${PUBDATE}" "${EPURL}"
       EpisodeTagging
-      CopyEpisode "${TANK}/${OUTFILE}"
+      CopyEpisode "${TANK_LOCAL}/${OUTFILE}"
       [ ! ${NO_SLACK} ] && AnnounceEpisode
 
-    else [ -f "${TANK}/${OUTFILE}" ]
+    else [ -f "${TANK_LOCAL}/${OUTFILE}" ]
       [ ${DEBUG} ] && echo "HAVE: ${OUTFILE}"
 
-      touch -d "$(date -d "${PUBDATE}" +%Y-%m-%d)" "${TANK}/${OUTFILE}"
-      CopyEpisode "${TANK}/${OUTFILE}"
+      touch -d "$(date -d "${PUBDATE}" +%Y-%m-%d)" "${TANK_LOCAL}/${OUTFILE}"
+      CopyEpisode "${TANK_LOCAL}/${OUTFILE}"
     fi
 
     UnsetThese
@@ -263,6 +238,7 @@ function WriteFeed() {
   curl -sL "${URL_RSS}" | xmllint --format --output "/tmp/${GENERIC_NAME}.xml" -
   ITEM_COUNT=$(xmllint --format --xpath "count(//item)" "/tmp/${GENERIC_NAME}.xml")
 }
+
 
 
 function GetItem() {
@@ -289,7 +265,7 @@ function DumpFound() {
   [ "${IMAGE}" ] && echo -e "\\tIMAGE: ${IMAGE}"
   [ "${OUTFILE}" ] && echo -e "\\tOUTFILE: ${OUTFILE}"
   [ "${PART}" ] && echo -e "\\tDO_RETAG: ${PART}"
-  [ "${PODCAST_ALBUM_ART}" ] && echo -e "\\PODCAST_ALBUM_ART: ${PODCAST_ALBUM_ART}"
+  [ "${PODCAST_ALBUM_ART}" ] && echo -e "\\tPODCAST_ALBUM_ART: ${PODCAST_ALBUM_ART}"
   [ "${PRETTY_NAME}" ] && echo -e "\\tPRETTY_NAME: ${PRETTY_NAME}"
   [ "${PUBDATE}" ] && echo -e "\\tPUBDATE: ${PUBDATE}"
   [ "${PUBEPOCH}" ] && echo -e "\\tPUBEPOCH: ${PUBEPOCH}"
