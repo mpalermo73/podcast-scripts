9916586605e64ea2a73e16e011eb66ee7a0abb95
Matt <matt.palermo@gmail.com>
2024-01-21 09:42:42 -0500

    converted

--- update-nocturne.sh
+++ update-nocturne.sh
@@ -7,11 +7,35 @@ GOOD_REGEX=""
 
 
 # DEBUG=TRUE
-# JUST_TEST=TRUE
+JUST_TEST=TRUE
 # NO_SLACK=TRUE
 # UPDATE_SYNCTHING=TRUE
+# NO_UPDATE_REMOTE=TRUE
 
 source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
+
+WriteFeed
+
+for ITEM in $(seq 1 ${ITEM_COUNT}) ; do
+
+  eval $(GetItem ${ITEM})
+
+  if [[ "${RAW_TITLE}" =~ ${GOOD_REGEX} ]] ; then
+
+    [ ${DEBUG} ] && echo "PASS regex: \"${RAW_TITLE}\""
+
+    TRACK=$(date -d "${PUBDATE}" +%y%m)
+
+    DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}" "${TRACK}"
+
+    UnsetThese
+
+  else
+    [ ${DEBUG} ] && echo "FAIL regex: \"${RAW_TITLE}\""
+  fi
+done
+
+exit
 
 CurlFeed
 
