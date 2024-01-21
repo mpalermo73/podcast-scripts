66285db5eecbc6b21e8accb40f969e6bcfb85a7d
Matt <matt.palermo@gmail.com>
2024-01-21 09:05:38 -0500

    converted

--- update-mission-rejected.sh
+++ update-mission-rejected.sh
@@ -2,18 +2,47 @@
 
 URL_RSS="https://feeds.simplecast.com/yMwA0_RF"
 PRETTY_NAME="Mission Rejected"
-GOOD_REGEX="[0-9]+"
+GOOD_REGEX="^[0-9]+"
 
 
 
 # DEBUG=TRUE
-# JUST_TEST=TRUE
+JUST_TEST=TRUE
 # NO_SLACK=TRUE
 # UPDATE_SYNCTHING=TRUE
+# NO_UPDATE_REMOTE=TRUE
 
 
 source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
 
+
+WriteFeed
+
+for ITEM in $(seq 1 ${ITEM_COUNT}) ; do
+
+  eval $(GetItem ${ITEM})
+
+  # DumpFound
+
+  if [[ "${RAW_TITLE}" =~ ${GOOD_REGEX} ]] ; then
+
+    [ ${DEBUG} ] && echo "PASS regex: \"${RAW_TITLE}\""
+
+    eval $(echo ${RAW_TITLE} | sed 's/\([0-9]\+\): \+\(.*\)/TRACK=\"\1\"\nTITLE=\"\2\"/')
+
+    TRACK="$(printf "%04d" ${TRACK})"
+
+    DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}" "${TRACK}"
+
+    UnsetThese
+
+  else
+    [ ${DEBUG} ] && echo "FAIL regex: \"${RAW_TITLE}\""
+  fi
+done
+
+
+exit
 CurlFeed
 
 for LINE in ${EPISODES} ; do
