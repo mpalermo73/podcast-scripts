cdb958bdd9c129bda3339fa78c1917b6e83213df
Matt <matt.palermo@gmail.com>
2024-01-21 09:32:23 -0500

    CONVERTED

--- update-nightvale.sh
+++ update-nightvale.sh
@@ -2,17 +2,50 @@
 
 URL_RSS="http://nightvale.libsyn.com/rss"
 PRETTY_NAME="Night Vale"
-GOOD_REGEX="^[0-9].*$"
+GOOD_REGEX="^[0-9]+ "
 
 
 
 # DEBUG=TRUE
 # JUST_TEST=TRUE
 # NO_SLACK=TRUE
-# UPDATE_SYNCTHING=TRUE
+UPDATE_SYNCTHING=TRUE
+# NO_UPDATE_REMOTE=TRUE
 
 
 source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
+
+WriteFeed
+
+for ITEM in $(seq 1 ${ITEM_COUNT}) ; do
+
+  eval $(GetItem ${ITEM})
+
+  DumpFound
+
+  if [[ "${RAW_TITLE}" =~ ${GOOD_REGEX} ]] ; then
+
+    [ ${DEBUG} ] && echo "PASS regex: \"${RAW_TITLE}\""
+
+    eval $(echo ${RAW_TITLE} | sed 's/\([0-9]\+\) \+[–-] \+\(.*\)/TRACK=\"\1\"\nTITLE=\"\2\"/')
+
+    echo "THIS: $TRACK | $TITLE"
+
+    # TRACK="$(printf "%04d" ${TRACK})"
+
+    DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}" "${TRACK}"
+
+    UnsetThese
+
+# exit
+
+  else
+    [ ${DEBUG} ] && echo "FAIL regex: \"${RAW_TITLE}\""
+  fi
+done
+
+
+exit
 
 CurlFeed
 
