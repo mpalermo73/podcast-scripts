b5542680de57ee6b100087a27acfd0b5a4c1e273
Matt <matt.palermo@gmail.com>
2024-01-29 17:27:25 -0500

    update-the-strata

---
+++ update-the-strata.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+URL_RSS="https://feeds.megaphone.fm/thestrata"
+PRETTY_NAME="The Strata"
+GOOD_REGEX="^[eE]pisode.*$"
+
+# DEBUG=TRUE
+# JUST_TEST=TRUE
+# NO_SLACK=TRUE
+# UPDATE_SYNCTHING=TRUE
+# NO_UPDATE_REMOTE=TRUE
+
+source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
+
+WriteFeed
+
+for ITEM in $(seq 1 ${ITEM_COUNT}) ; do
+
+  eval $(GetItem ${ITEM})
+
+  if [[ "${RAW_TITLE}" =~ ${GOOD_REGEX} ]] && [[ ! "${RAW_TITLE}" =~ [tT]railer ]] ; then
+
+    [ ${DEBUG} ] && echo "PASS regex: \"${RAW_TITLE}\""
+
+    eval $(echo ${RAW_TITLE} | sed 's/[eE]pisode \([0-9]\+\)\.\([0-9]\+\).*/SEASON="\1"\nTRACK="\2"/')
+
+    [ ${#SEASON} -eq 1 ] && SEASON="0${SEASON}"
+    [ ${#TRACK} -eq 1 ] && TRACK="0${TRACK}"
+
+    TRACK="${SEASON}${TRACK}"
+
+    DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}" "${TRACK}"
+
+    UnsetThese
+
+  else
+    [ ${DEBUG} ] && echo "FAIL regex: \"${RAW_TITLE}\""
+  fi
+done
