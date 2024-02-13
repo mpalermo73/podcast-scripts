6a1b5709deb89254653e195cb455376a25d42ca5
Matt <matt.palermo@gmail.com>
2024-02-12 20:36:26 -0500

    no syncting

--- update-midnight-burger.sh
+++ update-midnight-burger.sh
@@ -7,7 +7,7 @@ GOOD_REGEX="^[cC]hapter [0-9]+"
 DEBUG=TRUE
 # JUST_TEST=TRUE
 NO_SLACK=TRUE
-UPDATE_SYNCTHING=TRUE
+# UPDATE_SYNCTHING=TRUE
 # NO_UPDATE_REMOTE=TRUE
 
 source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
