cfc4048d39d0e9df295cb4b8e1b5363eb1f5d74d
Matt <matt.palermo@gmail.com>
2025-05-28 19:32:32 -0400

    convert olf59 to new file

--- update-wolf359.sh
+++ update-wolf359.sh
@@ -4,42 +4,33 @@ URL_RSS="http://wolf359radio.libsyn.com/rss"
 PRETTY_NAME="Wolf 359"
 GOOD_REGEX="^Episode [0-9]+: "
 
-
-
 DEBUG=TRUE
-JUST_TEST=TRUE
+# JUST_TEST=TRUE
 NO_SLACK=TRUE
-# NO_UPDATE_SYNCTHING=TRUE
-# NO_UPDATE_REMOTE=TRUE
+NO_UPDATE_SYNCTHING=TRUE
+NO_UPDATE_REMOTE=TRUE
 
 
 source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
 
-CurlFeed
+WriteFeed
 
-for LINE in ${EPISODES} ; do
+for ITEM in $(seq 1 ${ITEM_COUNT}) ; do
 
-  eval "${LINE}"
+  eval $(GetItem ${ITEM})
 
-  if [ "${PUBDATE}" -a "${EPURL}" -a "${TITLE}" ] ; then
-    if [[ "${TITLE}" =~ ${GOOD_REGEX} ]] && [ "${TYPE}" == "full" ] ; then
-      [ ${DEBUG} ] && echo "PASS regex: \"${TITLE}\""
+  if [[ "${RAW_TITLE}" =~ ${GOOD_REGEX} ]] && [ "${TYPE}" == "full" ] ; then
+    [ ${DEBUG} ] && echo "PASS regex: \"${RAW_TITLE}\""
 
-      # | sed 's/.*Episode.\([0-9]\+\):.\(.*\)/EPISODE="\1"\nTITLE="\2"/'
+    eval $(echo "${TITLE}" | sed 's/.*Episode.\([0-9]\+\):.\(.*\)/EPISODE="\1"\nTITLE="\2"/')
 
-      # [ ${#SEASON} -eq 1 ] && SEASON="0${SEASON}"
-      # [ ${#EPISODE} -eq 1 ] && EPISODE="0${EPISODE}"
+    TITLE="${EPISODE} - ${RAW_TITLE}"
 
-      eval $(echo "${TITLE}" | sed 's/.*Episode.\([0-9]\+\):.\(.*\)/EPISODE="\1"\nTITLE="\2"/')
+    DisectInfo "${PUBDATE}" "${EPURL}" "${RAW_TITLE}"
 
-      TITLE="${EPISODE} - ${TITLE}"
-
-      DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}"
-
-    else
-      [ ${DEBUG} ] && echo "FAIL regex: \"${TITLE}\""
-    fi
-    UnsetThese
+  else
+    [ ${DEBUG} ] && echo "FAIL regex: \"${RAW_TITLE}\""
   fi
+  UnsetThese
 
 done
