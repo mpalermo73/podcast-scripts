4626905ff148b7aa9ac026150df2180a598842cd
Matt <matt.palermo@gmail.com>
2025-06-02 12:30:31 -0400

    newer magic

--- update-moonbase-theta-out.sh
+++ update-moonbase-theta-out.sh
@@ -4,10 +4,10 @@ URL_RSS="https://feeds.megaphone.fm/moonbasethetaout"
 PRETTY_NAME="Moonbase Theta Out"
 GOOD_REGEX="^MTO Season [0-9]"
 
-DEBUG=TRUE
+# DEBUG=TRUE
 # JUST_TEST=TRUE
-NO_SLACK=TRUE
-# NO_UPDATE_SYNCTHING=TRUE
+# NO_SLACK=TRUE
+NO_UPDATE_SYNCTHING=TRUE
 # NO_UPDATE_REMOTE=TRUE
 
 source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
@@ -18,20 +18,37 @@ for ITEM in $(seq 1 ${ITEM_COUNT}) ; do
 
   eval $(GetItem ${ITEM})
 
-  if [ "${TYPE}" == "full" ] && [[ "${RAW_TITLE}" =~ ${GOOD_REGEX} ]] && [ "${TRACK}" ] ; then
+  PUB_YEAR=$(date -d "${PUBDATE}" +%y)
+
+  if [ "${TYPE}" == "full" ] ; then
 
     [ ${DEBUG} ] && echo "PASS regex: \"${RAW_TITLE}\""
 
-    eval $(echo "${RAW_TITLE}" | sed 's/.*: \“\(.*\)\”.*/TITLE=\"\1\"/')
+    # LAST_YEAR=0
 
-    [ ${#SEASON} -eq 1 ] && SEASON="0${SEASON}"
-    [ ${#TRACK} -eq 1 ] && TRACK="0${TRACK}"
+    if [ "${PUB_YEAR}" -gt "${LAST_YEAR}" ] ; then
 
-    TRACK="${SEASON}${TRACK}"
+      [ ${DEBUG} ] && echo -e "\n\n--------------------------- START OF TRACK ${TRACK_COUNTING} (${ITEM} of ${ITEM_COUNT}) ---------------------------"
 
-    DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}" "${TRACK}"
+      [ ! ${TRACK_COUNTING} ] && TRACK_COUNTING=$(yq --input-format xml --output-format json /tmp/MoonbaseThetaOut.xml | sed 's/"+@\?/"/g' | jq '[.rss.channel.item[] | select(.episodeType == "full")]' | jq length)
 
-    UnsetThese
+      TRACK=${TRACK_COUNTING}
+      [[ ${#TRACK} -le 2 ]] && TRACK="0${TRACK}"
+      [[ ${#TRACK} -le 1 ]] && TRACK="00${TRACK}"
+
+      eval $(echo "${RAW_TITLE}" | sed 's/.*: \“\(.*\)\”.*/TITLE=\"\1\"/ ; s/.*MTO Phases - \(Story [0-9]\+: .*\)<\/title>/TITLE="\1"/')
+
+      DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}" "${TRACK}"
+
+      UnsetThese
+
+      [ ${DEBUG} ] && echo "--------------------------- END OF TRACK ${TRACK_COUNTING} (${ITEM} of ${ITEM_COUNT}) ---------------------------"
+
+      TRACK_COUNTING=$(echo "${TRACK_COUNTING} - 1" | bc)
+
+    else
+      [ ${DEBUG} ] && echo "Skipping old episode: ${RAW_TITLE} (${PUB_YEAR} <= ${LAST_YEAR})"
+    fi
 
   else
     [ ${DEBUG} ] && echo "FAIL regex: \"${RAW_TITLE}\""
