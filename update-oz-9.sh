1ce85ee073a55a49f90d0e4207526c1e478373d8
Matt <matt.palermo@gmail.com>
2025-06-02 09:05:16 -0400

    use total track count

--- update-oz-9.sh
+++ update-oz-9.sh
@@ -8,7 +8,7 @@ GOOD_REGEX="^[eE]pisode.*$"
 # DEBUG=TRUE
 # JUST_TEST=TRUE
 # NO_SLACK=TRUE
-NO_UPDATE_SYNCTHING=TRUE
+# NO_UPDATE_SYNCTHING=TRUE
 # NO_UPDATE_REMOTE=TRUE
 
 source $HOME/GIT/podcast-scripts/update-podcasts-common.sh
@@ -19,19 +19,36 @@ for ITEM in $(seq 1 ${ITEM_COUNT}) ; do
 
   eval $(GetItem ${ITEM})
 
+  PUB_YEAR=$(date -d "${PUBDATE}" +%y)
+
   if [[ "${RAW_TITLE}" =~ ${GOOD_REGEX} ]] ; then
 
     [ ${DEBUG} ] && echo "PASS regex: \"${RAW_TITLE}\""
 
-    eval $(echo "${RAW_TITLE}" | sed 's/episode \([a-zA-Z].*\): \+\(.*\)/TRACK=\"\1\"\nTITLE=\"\2\"/ ; s/&amp;/\&/') 2>/dev/null
 
-    [ ! "${TRACK}" -o ! "${TITLE}" ] && eval $(echo "${RAW_TITLE}" | sed 's/episode \([0-9]\+\).*: \+\(.*\)/TRACK=\"\1\"\nTITLE=\"\2\"/')
+    if [ "${PUB_YEAR}" -gt "${LAST_YEAR}" ] ; then
 
-    TRACK=$(${HERE}/w2n.pl "${TRACK}")
+      [ ${DEBUG} ] && echo -e "\n\n--------------------------- START OF TRACK ${TRACK_COUNTING} (${ITEM} of ${ITEM_COUNT}) ---------------------------"
 
-    DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}" "${TRACK}"
+      [ ! ${TRACK_COUNTING} ] && TRACK_COUNTING=$(yq --input-format xml --output-format json /tmp/Oz9.xml | sed 's/"+@\?/"/g' | jq '[.rss.channel.item[] | select(.episodeType == "full")]' | jq length)
 
-    UnsetThese
+      TRACK=${TRACK_COUNTING}
+      [[ ${#TRACK} -le 2 ]] && TRACK="0${TRACK}"
+      [[ ${#TRACK} -le 1 ]] && TRACK="00${TRACK}"
+
+      eval $(echo "$RAW_TITLE" | sed 's/.*: \(.*\)$/TITLE="\1"/')
+
+      DisectInfo "${PUBDATE}" "${EPURL}" "${TITLE}" "${TRACK}"
+
+      UnsetThese
+
+      TRACK_COUNTING=$(echo "${TRACK_COUNTING} - 1" | bc)
+
+      [ ${DEBUG} ] && echo "--------------------------- END OF TRACK ${TRACK_COUNTING} (${ITEM} of ${ITEM_COUNT}) ---------------------------"
+
+    else
+      [ ${DEBUG} ] && echo "Skipping old episode: ${RAW_TITLE} (${PUB_YEAR} <= ${LAST_YEAR})"
+    fi
 
   else
     [ ${DEBUG} ] && echo "FAIL regex: \"${RAW_TITLE}\""
