9a02490df920719c4d034e1273215d7324fe96b0
Matt <matt.palermo@gmail.com>
2025-06-04 11:49:36 -0400

    not to self

--- update-trailer-park-boys.sh
+++ update-trailer-park-boys.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+# -> % for YEAR in $(jq -r '.rss.channel.item[].pubDate | strptime("%a, %d %b %Y %H:%M:%S %z") | strftime("%Y")' ~/Desktop/TrailerParkBoys.json | uniq) ; do jq --arg YEAR $YEAR '[.rss.channel.item[] | select(.pubDate | contains($YEAR))]' ~/Desktop/TrailerParkBoys.json | jq length ; done
+
+
 URL_RSS="https://tpbpodcast.libsyn.com/rss"
 PRETTY_NAME="Trailer Park Boys"
 # GOOD_REGEX="^[0-9].*$"
 
-# DEBUG=TRUE
-# JUST_TEST=TRUE
-# NO_SLACK=TRUE
-# NO_UPDATE_SYNCTHING=TRUE
-# NO_UPDATE_REMOTE=TRUE
+DEBUG=TRUE
+JUST_TEST=TRUE
+NO_SLACK=TRUE
+NO_UPDATE_SYNCTHING=TRUE
+NO_UPDATE_REMOTE=TRUE
 
 
 source $HOME/GIT/podcast-scripts/common-functions.sh
