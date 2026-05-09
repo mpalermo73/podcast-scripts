3c9004e93cc841433fdd2b5d0242542ae13e5bc2
Matt <matt.palermo@gmail.com>
2026-05-09 11:07:18 -0400

    enable DEBUG mode and uncomment update flags in update-the-byron-cronicles.sh

--- update-the-byron-cronicles.sh
+++ update-the-byron-cronicles.sh
@@ -4,11 +4,11 @@ URL_RSS="https://ericbusbypresents.com/category/the-byron-chronicles/feed/"
 PRETTY_NAME="The Byron Chronicles"
 GOOD_REGEX="^[eE]pisode [0-9]+.[0-9]+"
 
-# DEBUG=TRUE
+DEBUG=TRUE
 # JUST_TEST=TRUE
 NO_SLACK=TRUE
-# NO_UPDATE_SYNCTHING=TRUE
-# NO_UPDATE_REMOTE=TRUE
+NO_UPDATE_SYNCTHING=TRUE
+NO_UPDATE_REMOTE=TRUE
 
 source $HOME/GIT/podcast-scripts/common-functions.sh
 
